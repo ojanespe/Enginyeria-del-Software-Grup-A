8a56d0f8d8dacc07f975a5c84e139f0ea05478fc
marieges <marieges@PC>
2012-11-29 16:02:13 +0100

    Menu Modificado

--- TestJoc/src/menu/MenuPrincipal.java
+++ TestJoc/src/menu/MenuPrincipal.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package menu;
 
 import com.jme3.app.Application;
@@ -19,8 +15,12 @@ import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.elements.render.ImageRenderer;
+import de.lessvoid.nifty.render.NiftyImage;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,6 +40,8 @@ public class MenuPrincipal extends AbstractAppState implements ScreenController,
     private Nifty nifty;
     private Screen screen;
     private boolean isRunningMenuPrincipal = true;
+    private int indexPersonaje = 0;
+    private ArrayList<String> playersImages;
     
     public MenuPrincipal(SimpleApplication app){
         this.rootNode      = app.getRootNode();
@@ -53,17 +55,23 @@ public class MenuPrincipal extends AbstractAppState implements ScreenController,
         super.initialize(stateManager, app); 
         this.app = (SimpleApplication)app;   
         
+        playersImages = new ArrayList<String>();
+        playersImages.add("Interface/PlayersMenu/player1.png");
+        playersImages.add("Interface/PlayersMenu/player2.png");
+        playersImages.add("Interface/PlayersMenu/player3.png");
+        
         //Menu
         NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
                                                       this.app.getInputManager(),
                                                       this.app.getAudioRenderer(),
                                                       this.app.getGuiViewPort());
         nifty = niftyDisplay.getNifty();
-        nifty.fromXml("Interface/MenuPrincipal/MPrincipal.xml", "start",this);
+        nifty.fromXml("Interface/MainMenu/InitialMenu.xml", "start",this);
 
         // attach the nifty display to the gui view port as a processor
         app.getGuiViewPort().addProcessor(niftyDisplay);
         this.app.getInputManager().setCursorVisible(true);
+        
    } 
     
     /*
@@ -74,9 +82,13 @@ public class MenuPrincipal extends AbstractAppState implements ScreenController,
     }
     
     public void newGame(){
-       nifty.gotoScreen("end");
-       this.app.getInputManager().setCursorVisible(false);
-       this.setIsRunningMenuPrincipal(false);
+        nifty.gotoScreen("choosePlayer");
+    }
+    
+    public void acceptChoosePlayer() {
+        nifty.gotoScreen("end");
+        this.app.getInputManager().setCursorVisible(false);
+        this.setIsRunningMenuPrincipal(false);
     }
     
     public void exit(){
@@ -96,6 +108,11 @@ public class MenuPrincipal extends AbstractAppState implements ScreenController,
        
     }
     
+    public String getPlayerImage() {
+        System.out.println((String) playersImages.get(indexPersonaje));
+        return (String) playersImages.get(indexPersonaje);
+    }
+    
     public boolean getIsRunningMenuPrincipal(){
         return this.isRunningMenuPrincipal;
     }
@@ -103,4 +120,25 @@ public class MenuPrincipal extends AbstractAppState implements ScreenController,
         this.isRunningMenuPrincipal = IsRunning;
     }
     
+    public void prevPerson() {
+        indexPersonaje--;
+        if(indexPersonaje == 0) {
+            indexPersonaje = playersImages.size()-1;
+        }
+        cambioImagenPersonaje();
+    }
+    
+    public void nextPerson() {
+        indexPersonaje++;
+        if(indexPersonaje == playersImages.size()) {
+            indexPersonaje = 0;
+        }
+        cambioImagenPersonaje();
+    }
+    
+    private void cambioImagenPersonaje() {
+        NiftyImage newImage = nifty.getRenderEngine().createImage((String) playersImages.get(indexPersonaje), false); // false means don't linear filter the image, true would apply linear filtering
+        Element element = screen.findElementByName("personImage");
+        element.getRenderer(ImageRenderer.class).setImage(newImage);
+    }
 }
